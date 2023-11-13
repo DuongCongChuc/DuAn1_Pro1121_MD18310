49bf5b7b385747509a84eca9a0d1b306e70bea18
unknown <loindph36082@fpt.edu.vn>
2023-11-13 10:20:12 +0700

    Tải code

---
+++ N12_Pro1121_MD18310/app/src/main/java/com/example/n12_pro1121_md18310/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.n12_pro1121_md18310;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
